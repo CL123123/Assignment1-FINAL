89ff082d72434cd175010dcb8367a760aec4c3ca
DonaldZhong <donaldz@sfu.ca>
2015-02-01 18:47:53 -0800

    --

--- dist/scripts/controllers/about.js
+++ dist/scripts/controllers/about.js
@@ -1,4 +1,4 @@
 app.controller('AboutController', function($scope) {
   console
-  $scope.message = 'The SIAT quiz app has been created by students within the Interactive Arts and Technology program at SFU. Their course IAT 381 (mobile design/computing) has allowed them to gain the awesome skills necessary to create a simple yet elegant app for their first assignment.';
+  $scope.message1 = 'The SIAT quiz app has been created by students within the Interactive Arts and Technology program at SFU. Their course IAT 381 (mobile design/computing) has allowed them to gain the awesome skills necessary to create a simple yet elegant app for their first assignment.';
 });
--- dist/scripts/controllers/contact.js
+++ dist/scripts/controllers/contact.js
@@ -1,5 +1,7 @@
 app.controller('ContactController', function($scope) {
   console
     $scope.message = 'Donald Zhong - 301167293 - Donaldz@sfu.ca';
+    $scope.L = 'https://github.com/DonaldZhong';
     $scope.message2 = 'Delaram Fathi - 301179091 - Dfathi@sfu.ca';
+    $scope.L2 = 'https://github.com/df374';
 });

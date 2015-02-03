603c1df066e95583594d040e4f11c414b7f5d3aa
DonaldZhong <donaldz@sfu.ca>
2015-02-02 22:21:43 -0800

    --

--- dist/scripts/controllers/main.js
+++ dist/scripts/controllers/main.js
@@ -1,25 +1,3 @@
 app.controller('MainController', function($scope) {
   console
 });
-// app.controller('MainController', function($scope, $timeout) {
-//   $scope.disableBttn = function() {
-//     return $timeout(angular.noop, 1000);
-//   }
-// });
-
-// app.directive('clickAndDisable', function() {
-//   return {
-//     scope: {
-//       clickAndDisable: '&'
-//     },
-//     link: function(scope, iElement, iAttrs) {
-//       iElement.bind('click', function() {
-//         iElement.prop('disabled',true);
-//         scope.clickAndDisable().finally(function() {
-//           iElement.prop('disabled',false);
-//         })
-//       });
-//     }
-//   };
-// });
-
